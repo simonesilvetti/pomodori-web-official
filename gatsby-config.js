40f8ff8b5d0ac48ca054a0c89f1477387ec501ac
ssilvetti <silvetti@esteco.com>
2021-03-17 23:04:07 +0100

    add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    `gatsby-plugin-sitemap`,
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
